6bbc4de3ae512650b59a6ee1e2352caefea737ec
Philippe Joseph <p.joseph@simplytest.de>
2025-04-08 15:56:22 +0200

    add a function library

---
+++ cypress/e2e/FunctionLibraries/ResponseUtility.ts
@@ -0,0 +1,40 @@
+﻿class ResponseUtility
+{
+
+    waitForFailureResponseOf(requestName: string, expectedStatusCode: number)
+    {
+        cy.wait("@" + requestName).then((interception) =>
+        {
+            expect(interception.response.statusCode).to.equal(expectedStatusCode);
+            //expect(interception.request.url).to.contain(endpoint);
+            expect(interception.request.method).to.equal("POST");
+        });
+    }
+
+    waitForSuccessfulResponseOf(requestName: string)
+    {
+        cy.wait("@" + requestName).then((interception) =>
+        {
+            expect(interception.response.statusCode).to.equal(200);
+        });
+    }
+    verifyResponseOf(alias: string, callback: (response: any) => void)
+    {
+        cy.wait(`@${alias}`).then((interception) =>
+        {
+            callback(interception.response);
+        });
+    }
+
+    verifySuccessfulResponseOf(alias: string)
+    {
+        cy.wait(`@${alias}`).its("response.statusCode").should("eq", 200);
+    }
+
+    verifyFailedResponseOf(alias: string, statusCode: number)
+    {
+        cy.wait(`@${alias}`).its("response.statusCode").should("eq", statusCode);
+    }
+}
+
+export default new ResponseUtility();
