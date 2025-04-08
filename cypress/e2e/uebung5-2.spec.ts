5a1c1369e93f3f5284e6274659f25b16250ad160
Philippe Joseph <p.joseph@simplytest.de>
2025-04-08 11:19:59 +0200

    add register with mocked backend using fixtures

---
+++ cypress/e2e/uebung5-2.spec.ts
@@ -0,0 +1,52 @@
+describe("Übung 5-2, Registeriung ohne Backend", () =>
+{
+    const dashboardUrl = "http://localhost:4200/dashboard";
+
+    beforeEach(() =>
+    {
+        // Laden der Registeriungsdaten
+        cy.fixture("registrationData").as("registrationData");
+        cy.fixture("contracts").as("contractsData");
+        cy.visit("/");
+        cy.get("h1").should("contain", "Smart Money");
+        cy.url().should("eq", dashboardUrl);
+    });
+
+    it("5.2.1 Erfolgreiche Registrierung ohne Interception - benötigt Backend", function ()
+    {
+        // Zugriff auf die Fixture-Daten im Testfall
+        const registrationData = this.registrationData;
+
+        // Klicken Sie auf den Registrierungsbutton
+        cy.get("#registerButton").click();
+
+        // Erfassen Sie den Netzwerkverkehr für die Registrierung
+        cy.intercept("POST", "**/api/contracts", {
+            statusCode: 201,
+            body: registrationData.registrationResponse,
+        }).as("registerRequest");
+
+        cy.intercept("GET", "**/api/contracts", {
+            statusCode: 200,
+            body: this.contractsData.initial,
+        }).as("contractsRequest");
+
+        // Füllen Sie das Registrierungsformular mit den Fixture-Daten aus
+        cy.get("#firstName").type(registrationData.firstName);
+        cy.get("#lastName").type(registrationData.lastName);
+        cy.get("#password").type(registrationData.password);
+        cy.get("#street").type(registrationData.street);
+        cy.get("#house").type(registrationData.house);
+        cy.get("#zipCode").type(registrationData.zipCode);
+        cy.get("#city").type(registrationData.city);
+        cy.get("#country").type(registrationData.country);
+        cy.get("#email").type(registrationData.email);
+        cy.get("#birthDay").type(registrationData.birthDay);
+
+        // Überprüfen Sie, ob das Submit-Button aktiviert ist und klicken Sie sie an
+        cy.get("button[data-testid='register_button']").should("be.enabled").click();
+        cy.get("label[data-testid=\"customer_Label\"]").should("contain", "Max");
+        cy.url().should("contain", "mainPage");
+    });
+
+});
