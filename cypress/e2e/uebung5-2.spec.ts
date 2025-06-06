c77ea24c95cd917ae7187e54f84586df2788fe9a
Philippe Joseph <p.joseph@simplytest.de>
2025-06-06 10:17:36 +0200

    add exercise 5-2

--- cypress/e2e/uebung5-2.spec.ts
+++ cypress/e2e/uebung5-2.spec.ts
@@ -50,4 +50,44 @@ describe("Übung 5-2, Registeriung ohne Backend", () =>
         cy.get("label[data-testid=\"customer_Label\"]").should("contain", "Max");
         cy.url().should("contain", "mainPage");
     });
+
+    it.only("Gescheiterte Registrierung wegen Minderjährigkeit", function ()
+    {
+        // Zugriff auf die Fixture-Daten im Testfall
+        const registrationData = this.registrationData;
+
+        // Erfassen Sie den Netzwerkverkehr für die fehlgeschlagene Registrierung wegen Minderjährigkeit
+        cy.intercept("POST", "/api/contracts", {
+            statusCode: 400,
+            body: registrationData.underageRegistrationResponse,
+        }).as("underageRegisterRequest");
+
+        // Klicken Sie auf den Registrierungsbutton
+        cy.get("#registerButton").click();
+        // Füllen Sie das Registrierungsformular mit den Fixture-Daten aus
+        cy.get("#firstName").type(registrationData.firstName);
+        cy.get("#lastName").type(registrationData.lastName);
+        cy.get("#password").type(registrationData.password);
+        cy.get("#street").type(registrationData.street);
+        cy.get("#house").type(registrationData.house);
+        cy.get("#zipCode").type(registrationData.zipCode);
+        cy.get("#city").type(registrationData.city);
+        cy.get("#country").type(registrationData.country);
+        cy.get("#email").type(registrationData.email);
+        cy.get("#birthDay").type(registrationData.birthDay);
+        // Fügen Sie einen Listener für das `window:alert`-Ereignis hinzu
+        const alertStub = cy.stub();
+        cy.on("window:alert", alertStub);
+
+        cy.get("button[data-testid='register_button']").should("be.enabled").click().then(() =>
+        {
+            expect(alertStub).to.have.been.calledWith("Underage");
+        });
+        cy.get("button[data-testid='register_button']").should("be.enabled").click();
+        cy.wait("@underageRegisterRequest").should((xhr) =>
+        {
+            expect(xhr.response.statusCode).to.equal(400);
+            expect(xhr.response.body.error).to.equal("Underage");
+        });
+    });
 });
